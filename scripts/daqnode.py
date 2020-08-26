6c3b441f5d2a7df34f1d2f9357ce8831d3f785b6
Daniel Hetzel <hetzel.d@northeastern.edu>
2020-08-25 18:35:59 -0700

    made filtering happen inside daq node

--- scripts/daqnode.py
+++ scripts/daqnode.py
@@ -9,14 +9,18 @@ from uldaq import (get_daq_device_inventory, DaqDevice, InterfaceType,
                    AiInputMode, AInFlag)
 
 import rospy
-from ur5teleop.msg import daqdata
-
+from ur5teleop.msg import daqdata, jointdata
+from math import tan, pi
 
 def talker():
-    pub = rospy.Publisher('daqdata_raw', daqdata, queue_size=1)
+    pub = rospy.Publisher('daqdata_filtered', jointdata, queue_size=1)
     rospy.init_node('daqnode')
     fs=rospy.get_param("/frequency/sample", default=100.0) # default sample freqency in hz unless defined presartup parameter
     rospy.set_param("/frequency/sample",fs)
+    rospy.set_param("/frequency/corner", 5.0) # default corner frequency
+
+
+
     rate = rospy.Rate(fs)
 
     daq_device = None
@@ -96,16 +100,23 @@ def talker():
         # system('clear')
         print('connected to DAQ and starting publish node')
         while not rospy.is_shutdown():
-            sptime= rospy.Time.now()
-            ref= ai_device.a_in(0, input_mode, ranges[range_index],AInFlag.DEFAULT)
-            ch1= ai_device.a_in(1, input_mode, ranges[range_index],AInFlag.DEFAULT)   
-            ch2= ai_device.a_in(2, input_mode, ranges[range_index],AInFlag.DEFAULT)   
-            ch3= ai_device.a_in(3, input_mode, ranges[range_index],AInFlag.DEFAULT)   
-            ch4= ai_device.a_in(4, input_mode, ranges[range_index],AInFlag.DEFAULT)   
-            ch5= ai_device.a_in(5, input_mode, ranges[range_index],AInFlag.DEFAULT)   
-            ch6= ai_device.a_in(6, input_mode, ranges[range_index],AInFlag.DEFAULT)         
-            # rospy.loginfo([ref, ch1])
-            pub.publish(sptime,ref,ch1,ch2,ch3,ch4,ch5,ch6)
+            class data:
+                sptime= rospy.Time.now()
+                ref= ai_device.a_in(0, input_mode, ranges[range_index],AInFlag.DEFAULT)
+                encoder1= ai_device.a_in(1, input_mode, ranges[range_index],AInFlag.DEFAULT)
+                encoder2= ai_device.a_in(2, input_mode, ranges[range_index],AInFlag.DEFAULT)
+                encoder3= ai_device.a_in(3, input_mode, ranges[range_index],AInFlag.DEFAULT)
+                encoder4= ai_device.a_in(4, input_mode, ranges[range_index],AInFlag.DEFAULT)
+                encoder5= ai_device.a_in(5, input_mode, ranges[range_index],AInFlag.DEFAULT)
+                encoder6= ai_device.a_in(6, input_mode, ranges[range_index],AInFlag.DEFAULT)
+
+            angles_wrapped = angles(data)
+            angles_unwrapped = unwrap(angles_wrapped)  # generate unwrapped angles
+            dt = getdt(data.sptime)
+            angles_filtered = lowpass2(angles_unwrapped, fs)
+            angular_vels = getvelocity(angles_filtered, dt)
+            message = pubprep(angles_filtered, angular_vels, data.sptime, dt)
+            pub.publish(message)
             rate.sleep()
 
     finally:
@@ -113,7 +124,149 @@ def talker():
             if daq_device.is_connected():
                 daq_device.disconnect()
             daq_device.release()
-        
+
+def getdt(sptime):
+    try:
+        prev_sptime=getdt.prev
+    except:
+        dtsecs=0
+    else:
+        dt=sptime-prev_sptime
+        dtsecs = dt.secs + dt.nsecs * 1e-9
+    getdt.prev=sptime
+    return dtsecs
+
+
+def pubprep(angles,vels,sptime,dtsecs):
+    msg=jointdata()
+
+    msg.sptime=sptime
+    msg.dt=dtsecs
+    msg.encoder1.pos=angles[0]
+    msg.encoder1.vel=vels[0]
+
+    msg.encoder2.pos=angles[1]
+    msg.encoder2.vel=vels[1]
+
+    msg.encoder3.pos=angles[2]
+    msg.encoder3.vel=vels[2]
+
+    msg.encoder4.pos=angles[3]
+    msg.encoder4.vel=vels[3]
+
+    msg.encoder5.pos=angles[4]
+    msg.encoder5.vel=vels[4]
+
+    msg.encoder6.pos=angles[5]
+    msg.encoder6.vel=vels[5]
+
+    return msg
+
+
+def getvelocity(angles, dt):
+    try:
+        prev_angles=getvelocity.prev
+    except:
+        velocity=[0]*6
+    else:
+        velocity=[]
+        for i in range(6):
+            dtheta=angles[i]-prev_angles[i]
+            vel=dtheta/dt
+            velocity.append(vel)
+    getvelocity.prev=angles
+    return velocity
+
+
+def lowpass2(vals,fs):
+    fc_param = rospy.get_param('/frequency/corner')
+    if not hasattr(lowpass2,'fs'):
+        lowpass2.fs=fs
+        lowpass2.fc=fc_param
+        lowpass2.coeffs=filtercoeffs(lowpass2.fs,lowpass2.fc)
+
+    if fc_param != lowpass2.fc:
+        lowpass2.fc = fc_param
+        lowpass2.coeffs=filtercoeffs(lowpass2.fs,lowpass2.fc)
+
+    b,a=lowpass2.coeffs
+
+    try:
+        prev_filter_vals,prev_vals=lowpass2.prev
+    except:
+        prev_filter_vals=[vals,vals]
+        prev_vals=[vals,vals]
+    filtered_vals=[]
+    for i in range(6):
+        filtered=b[0]*vals[i]+ b[1]*prev_vals[0][i] + b[2]*prev_vals[1][i]- a[0]*prev_filter_vals[0][i] - a[1]*prev_filter_vals[1][i]
+        filtered_vals.append(filtered)
+    prev_filter_vals=[filtered_vals,prev_filter_vals[0]]
+    prev_vals=[vals,prev_vals[0]]
+    lowpass2.prev=(prev_filter_vals,prev_vals)
+    return filtered_vals
+
+
+def unwrap(angles_wrapped):
+    try:
+        prevangles_w, prevangles_uw = unwrap.prev  # if this fails it is the first iteration
+    except:
+        angles_unwrapped = angles_wrapped
+    else:
+        angles_unwrapped = []
+        for i in range(6):
+            dtheta = prevangles_w[i] - angles_wrapped[i]
+            if dtheta > pi:  # adding to rotation
+                change = 1
+            elif dtheta < -pi:  # subtracting from the rotation
+                change = -1
+            else:
+                change = 0
+            rots = int(prevangles_uw[i] / (2 * pi)) + change
+            if prevangles_uw[i] < 0:
+                rots -= 1
+            unwrapped = rots * 2 * pi + angles_wrapped[i]
+            if unwrapped > prevangles_uw[i] + pi:
+                rots -= 1
+            if unwrapped < prevangles_uw[i] - pi:
+                rots += 1
+            angles_unwrapped.append(rots * 2 * pi + angles_wrapped[i])
+    unwrap.prev = (angles_wrapped, angles_unwrapped)
+    return angles_unwrapped
+
+
+def angles(data):
+    ref = data.ref
+    maxv = ref - 0.015
+    minv = 0
+    voltages = [data.encoder1, data.encoder2, data.encoder3, data.encoder4, data.encoder5, data.encoder6]
+    angles = []
+
+    for volt in voltages:
+        ratio = (volt - minv) / (maxv - minv)
+        if ratio > 1:
+            ratio = 1
+        if ratio < 0:
+            ratio = 0
+        angle = ratio * 2 * pi
+        angles.append(angle)
+    return angles
+
+
+def filtercoeffs(fs, fc):
+    Ts=1/float(fs)
+    zeta = 2 ** 0.5 / 2
+    w0 = 2 * pi * fc
+    K = w0 / tan(w0 * Ts / 2)
+    phi = K ** 2 + 2 * zeta * w0 * K + w0 ** 2
+    b0 = w0 ** 2 / phi
+    b1 = 2 * b0
+    b2 = b0
+    a1 = (2 * w0 ** 2 - 2 * K ** 2) / phi
+    a2 = (K ** 2 - 2 * zeta * w0 * K + w0 ** 2) / phi
+
+    coeffs = ([b0, b1, b2], [a1, a2])
+    return coeffs
+
 if __name__ == '__main__':
     try:
         talker()
